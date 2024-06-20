73c0255f1ca292a545a5d82bdd58fba859f8da82
Felipe-DelosSantos09 <pipepro117@gmail.com>
2024-06-19 21:30:16 -0600

    Solución al ejercicio 6.2_Edades

---
+++ Modulo_1/6.2_Edades.py
@@ -0,0 +1,9 @@
+edad = int(input("Introduzca su edad: "))
+if edad < 13:
+    print("Eres un niño")
+elif edad < 18:
+    print("Eres un adolescente")
+elif edad < 65:
+    print("Eres un adulto")
+else:
+    print("Eres un adulto mayor")
